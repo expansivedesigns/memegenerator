591537169a39bee4238f7d58938bd07a893d84a7
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2019-12-14 20:03:28 -0500

    Updates

---
+++ precache-manifest.6fadaaaea120edfd53eb286fd7d66001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4427f12554246cb5fe64ca5444cf80fd",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "5f07d6df2b259c23f7c2",
+    "url": "/memegenerator/static/css/main.c0b91003.chunk.css"
+  },
+  {
+    "revision": "34e2a172fde37b73b445",
+    "url": "/memegenerator/static/js/2.449e5945.chunk.js"
+  },
+  {
+    "revision": "5f07d6df2b259c23f7c2",
+    "url": "/memegenerator/static/js/main.544b227e.chunk.js"
+  },
+  {
+    "revision": "4a1a25ad94ab04e22e81",
+    "url": "/memegenerator/static/js/runtime-main.0f1202e6.js"
+  }
+]);
